895668c3d7fd94e81db37b3f8c42119f8a0a6fd8
Firas Khalfaoui <firaskhalfaoui559@gmail.com>
2026-07-10 14:22:24 +0100

    Update ddl_bronzelayer.sql

--- scripts/bronze/ddl_bronzelayer.sql
+++ scripts/bronze/ddl_bronzelayer.sql
@@ -1,3 +1,13 @@
+/*
+===============================================================================
+DDL Script: Create Bronze Tables
+===============================================================================
+Script Purpose:
+    This script creates tables in the 'bronze' schema, dropping existing tables 
+    if they already exist.
+	  Run this script to re-define the DDL structure of 'bronze' Tables
+===============================================================================
+*/
 if object_id ('bronze.crm_cust_info','U') is not null 
     drop table bronze.crm_cust_info
 create table bronze.crm_cust_info(
