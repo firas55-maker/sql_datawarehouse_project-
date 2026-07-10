451da5ceb1b78e91d8dd021adcbdc5a5c99929bf
Firas Khalfaoui <firaskhalfaoui559@gmail.com>
2026-07-10 14:13:30 +0100

    Update ddl_bronzelayer.sql

--- scripts/bronze/ddl_bronzelayer.sql
+++ scripts/bronze/ddl_bronzelayer.sql
@@ -1,1 +1,57 @@
+if object_id ('bronze.crm_cust_info','U') is not null 
+    drop table bronze.crm_cust_info
+create table bronze.crm_cust_info(
+	cst_id int ,
+	cst_key nvarchar(50),
+	cst_firstname nvarchar(50),
+	cst_lastname nvarchar(50),
+	cst_marital_status nvarchar(50),
+	cst_gndr nvarchar(50),
+	cst_create_date date
+
+);
+
+go
+if object_id ('bronze.crm_prd_info','U') is not null 
+    drop table bronze.crm_prd_info
+CREATE TABLE bronze.crm_prd_info (
+    prd_id       INT,
+    prd_key      NVARCHAR(50),
+    prd_nm       NVARCHAR(50),
+    prd_cost     INT,
+    prd_line     NVARCHAR(50),
+    prd_start_dt DATETIME,
+    prd_end_dt   DATETIME
+);
+
+go
+
+CREATE TABLE bronze.crm_sales_details (
+    sls_ord_num  NVARCHAR(50),
+    sls_prd_key  NVARCHAR(50),
+    sls_cust_id  INT,
+    sls_order_dt INT,
+    sls_ship_dt  INT,
+    sls_due_dt   INT,
+    sls_sales    INT,
+    sls_quantity INT,
+    sls_price    INT
+);
+GO
+
+CREATE TABLE bronze.erp_cust_az12 (
+    cid    NVARCHAR(50),
+    bdate  DATE,
+    gen    NVARCHAR(50)
+);
+GO
+
+CREATE TABLE bronze.erp_px_cat_g1v2 (
+    id           NVARCHAR(50),
+    cat          NVARCHAR(50),
+    subcat       NVARCHAR(50),
+    maintenance  NVARCHAR(50)
+);
+GO
+
 
