e1bf34563bb45bbfa25f83724a034ebab89a5cea
Firas Khalfaoui <firaskhalfaoui559@gmail.com>
2026-07-10 14:22:04 +0100

    Create proc_loadbronze.sql

---
+++ scripts/bronze/proc_loadbronze.sql
@@ -0,0 +1,90 @@
+/*
+===============================================================================
+Stored Procedure: Load Bronze Layer (Source -> Bronze)
+===============================================================================
+Script Purpose:
+    This stored procedure loads data into the 'bronze' schema from external CSV files. 
+    It performs the following actions:
+    - Truncates the bronze tables before loading data.
+    - Uses the `BULK INSERT` command to load data from csv Files to bronze tables.
+
+Parameters:
+    None. 
+	  This stored procedure does not accept any parameters or return any values.
+
+Usage Example:
+    EXEC bronze.load_bronze;
+===============================================================================
+*/
+
+
+
+CREATE OR ALTER PROCEDURE bronze.load_bronze as 
+BEGIN 
+
+truncate table bronze.crm_cust_info;
+bulk insert bronze.crm_cust_info
+from 'C:\Users\LENOVO\Desktop\SQL\data warehouse project\sql-data-warehouse-project\datasets\source_crm\cust_info.csv'
+with (
+	firstrow=2,
+	FIELDTERMINATOR =',',
+	tablock
+);
+
+
+truncate table bronze.crm_sales_details;
+bulk insert bronze.crm_sales_details
+from 'C:\Users\LENOVO\Desktop\SQL\data warehouse project\sql-data-warehouse-project\datasets\source_crm\sales_details.csv'
+with (
+	firstrow=2,
+	FIELDTERMINATOR =',',
+	tablock
+);
+
+
+
+
+truncate table bronze.crm_prd_info;
+bulk insert bronze.crm_prd_info
+from 'C:\Users\LENOVO\Desktop\SQL\data warehouse project\sql-data-warehouse-project\datasets\source_crm\prd_info.csv'
+with (
+	firstrow=2,
+	FIELDTERMINATOR =',',
+	tablock
+);
+
+
+truncate table bronze.erp_cust_az12;
+bulk insert bronze.erp_cust_az12
+from 'C:\Users\LENOVO\Desktop\SQL\data warehouse project\sql-data-warehouse-project\datasets\source_erp\cust_az12.csv'
+with (
+	firstrow=2,
+	FIELDTERMINATOR =',',
+	tablock
+);
+
+
+
+truncate table bronze.erp_loc_a101;
+bulk insert bronze.erp_loc_a101
+from 'C:\Users\LENOVO\Desktop\SQL\data warehouse project\sql-data-warehouse-project\datasets\source_erp\loc_a101.csv'
+with (
+	firstrow=2,
+	FIELDTERMINATOR =',',
+	tablock
+);
+
+
+
+truncate table bronze.erp_px_cat_g1v2;
+bulk insert bronze.erp_px_cat_g1v2
+from 'C:\Users\LENOVO\Desktop\SQL\data warehouse project\sql-data-warehouse-project\datasets\source_erp\px_cat_g1v2.csv'
+with (
+	firstrow=2,
+	FIELDTERMINATOR =',',
+	tablock
+);
+END 
+
+
+
